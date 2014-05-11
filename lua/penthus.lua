63ccf31b18d9ab031759331d377e697e77cac67a
DaaOtt <daaott@gmail.com>
2014-05-11 14:03:42 -0700

    version v2

--- lua/penthus.lua
+++ lua/penthus.lua
@@ -3,7 +3,7 @@ setmetatable(newFenv, {__index = _G}) --copying _G into our new fenv
 setfenv(1, newFenv) --making our new fenv active for the rest of the file
 
 name = "penthus" --don't reference this, use penthus.getName()
-version = "0"
+
 
 include(name.."/lib/lib.lua")
 include(name.."/modules/modules.lua")
@@ -22,6 +22,7 @@ class "penthus" {
 
 	private {
 		name = name;
+		version = "0";
 	};
 }
 penthus = penthus.new()
