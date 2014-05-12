9503c08cc867852cf5528ff199afe50216a42837
LennyPenny <l.bernhardt@live.de>
2014-05-12 19:52:31 +0200

    added documentation

--- lua/penthus.lua
+++ lua/penthus.lua
@@ -5,16 +5,16 @@ setfenv(1, newFenv) --making our new fenv active for the rest of the file
 name = "penthus" --don't reference this, use penthus.getName()
 
 
-include(name.."/lib/lib.lua")
+include(name.."/lib/lib.lua") --loading critical, non SIMPLOO libs
 
 class "penthus" {
 	public {
 		static {
-			mods = {};
+			mods = {}; --keeping track of all modules we have in modules/ here
 		};
 
-		mod = {};
-		add = function(self, name, instance)
+		mod = {}; --all modules will be loaded into this
+		add = function(self, name, instance) --this is how we load modules into the penthus class (taken care of by modules/modules.lua)
 			self.mod[name] = instance
 		end;
 
@@ -31,7 +31,6 @@ class "penthus" {
 		version = "0";
 	};
 }
-
 penthus = penthus.new()
 
 include(name.."/modules/modules.lua")
--- lua/penthus/lib/lib.lua
+++ lua/penthus/lib/lib.lua
@@ -1,7 +1,7 @@
 setfenv(1, getfenv(3)) --setting the fenv to the one we created
 
 local libs, _ = file.Find(name.."/lib/*.lua", "lsv") --why is it 'lsv' and not 'LUA'??
-for k, v in pairs(libs) do
-	if v == "lib.lua" then continue end
-	include(name.."/lib/"..v)
+for k, lib in pairs(libs) do --loading our libs
+	if lib == "lib.lua" then continue end
+	include(name.."/lib/"..lib)
 end
