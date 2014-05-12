f0563e07a7d032c2c3b77f165bd89b01ca0a00f3
LennyPenny <l.bernhardt@live.de>
2014-05-12 20:13:42 +0200

    name is depracted after penthus is initialized

--- lua/penthus/modules/modules.lua
+++ lua/penthus/modules/modules.lua
@@ -1,9 +1,9 @@
 setfenv(1, getfenv(3)) --setting the fenv to the one we created
 
-local modules, _ = file.Find(name.."/modules/*.lua", "lsv") --why is it 'lsv' and not 'LUA'??
+local modules, _ = file.Find(penthus:getName().."/modules/*.lua", "lsv") --why is it 'lsv' and not 'LUA'??
 for k, modul in pairs(modules) do --loading the file and saving the names of the modules
 	if modul == "modules.lua" then continue end
-	include(name.."/modules/"..modul)
+	include(penthus:getName().."/modules/"..modul)
 
 	modul = string.Replace(modul, ".lua", "")
 	table.insert(penthus.mods, modul)
