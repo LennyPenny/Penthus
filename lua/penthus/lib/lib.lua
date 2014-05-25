899e1332d42486b487fb2a4f0733cd8764e726f2
DaaOtt <daaott@gmail.com>
2014-05-24 23:17:06 -0700

    I guess it was like that for a reason.

--- lua/penthus/lib/lib.lua
+++ lua/penthus/lib/lib.lua
@@ -1,7 +1,7 @@
 setfenv(1, getfenv(3)) --setting the fenv to the one we created
 
-local libs, _ = file.Find(penthus.getName().."/lib/*.lua", "lsv") --why is it 'lsv' and not 'LUA'??
+local libs, _ = file.Find(name.."/lib/*.lua", "lsv") --why is it 'lsv' and not 'LUA'??
 for k, lib in pairs(libs) do --loading our libs
 	if lib == "lib.lua" then continue end
-	include(penthus.getName().."/lib/"..lib)
+	include(name.."/lib/"..lib)
 end
